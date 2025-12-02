9c78756143022773c5b230c03a9840da406e8f7b
ritam-g <ritammaty2006@gmail.com>
2025-12-02 22:56:07 +0530

    call,apply,bind on js day 57

--- Java Script from day26(js parctice folder)/DOM -- START FROM DAY42/Day-57/script.js
+++ Java Script from day26(js parctice folder)/DOM -- START FROM DAY42/Day-57/script.js
@@ -1,97 +1,23 @@
+//! call,apply,bind
+//! when ever we want "this" value according to us we use these three options
 
-//! NORMAL JS CLASS STURCTURE 
-// class Student {
-//     constructor(name,roll,address) {
-//         this.name=name
-//         this.roll=roll
-//         this.address=address
-//     }
-//     studentDetails(){
-//         console.log(`Strudent name is ${this.name} \n roll : ${this.roll} \nadress: ${this.address}`);
-        
-//     }
-//     deleteStudentDetails(){
-//         this.name=null
-//            this.roll=null
-//            this.address=null
-//     }
-//     printLog(){
-//         console.log(this)
-//     }
-//     a=()=>{
-//         return this.studentDetails()
-//     }
-// }
-// let s1=new Student("ritam",60,"WestBengal")
-// s1.studentDetails()
-// s1.printLog()
-// s1.a()
+let obj = {
+    UserName: "ritam",
+    Adress: "WB"
+};
 
-//! REAL USE OF THIS KEY WORD AND GETSOME MEANING  
-
-/**======================
- *!    NORMAL THIS PRINTING
- *========================**/
-console.log(this);
-
-/**======================
- *!    function → window
- *========================**/
-function call() {
-    console.log(this); 
+function demo(...arr) {
+    console.log(this, arr.toString());
 }
-call()
 
-/**======================
- *!    ES5 function inside object → object
- *========================**/
+//! call()
+demo.call(obj, 1, 2, 3, 4, 5);
 
- let obj={
-    fun:function call() {
-        console.log(this);
-    },
-    address:"HydraBad"
- }
- obj.fun()
+//! apply()
+demo.apply(obj, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
- /**======================
-  *!    ES6 (arrow) inside object → window
-  *========================**/
- let obj2={
-    fun:() =>{
-        console.log(this);
-    },
-    address:"HydraBad"
- }
- obj2.fun()
+// bind()
 
- /**======================
-  *!    WES5 inside ES5 → window
-  *========================**/
-
-let obj3={
-    fun:function(){
-        function another() {
-            console.log(this);
-        }
-        another()
-    },
-    address:"HydraBad"
- }
- obj3.fun()
-
- /**======================
-  *!    ES6 inside ES5 inside object → object
-  *========================**/
- let obj4={
-    fun:function(){
-        let another=()=> {
-            console.log(this);
-        }
-        another()
-    },
-    address:"HydraBad"
- }
- obj4.fun()
-
- 
+//! bind() 
+let fun = demo.bind(obj, 1, 2, 34, 5, 567, 6);
+fun();
