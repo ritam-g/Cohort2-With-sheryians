9b8d8c5b3f99b4be802632a1b9fae0875f65b74b
ritam-g <ritammaty2006@gmail.com>
2025-12-10 18:06:02 +0530

    normal task done

---
+++ Java Script from day26(js parctice folder)/Class-and-Object-Day-56/Day-60/scriptTask.js
@@ -0,0 +1,40 @@
+//! TASK 1 
+// function afterDelay(time,cb) {
+//     setTimeout(() => {
+//         cb()
+//     }, time);
+// }
+// function call() {
+//     console.log("hello world");
+    
+// }
+// afterDelay(3000,call)
+//! task-2 
+// function getUser(userName,cb) {
+//     user={
+//     username:"ritam",
+//     group:"bca",
+//     userId:123
+//     }
+//     setTimeout(() => {
+//         cb(user,function (id,cb) {
+//             setTimeout(function() {
+//                 cb(["hellow","hi","by"])
+//             }, 1000);
+            
+//         })
+//     }, 1000);
+// }
+// getUser("ritam",(user,cb)=>{
+//     console.log(user);
+//     cb(user.userId,function (arr) {
+//         console.log(arr);
+        
+//     })
+        
+    
+    
+// })
+//! task 3
+
+
