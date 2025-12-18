e4a83ee9121412cfcc8c586f7d2d5afb51f8ce24
ritam-g <ritammaty2006@gmail.com>
2025-12-18 23:01:24 +0530

    vite+react leanrning day 68

---
+++ REACT/Day-68/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>hello welcome to the react world</h1>
+      <h1>hello welcome to the react world</h1>
+      
+    </div>
+  )
+}
+
+export default App
