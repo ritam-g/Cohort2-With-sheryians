1d1c729e6212c7c93e6ab5acb3e40c7a21045299
ritam-g <ritammaty2006@gmail.com>
2025-12-20 22:01:56 +0530

    day-7- leraning about tailwind

---
+++ REACT/Day70(Tailwind)/tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-500 h-[20rem] w-[20rem] p-12 border-black border-8 flex items-center justify-center m-[1rem]">
+      <h1 className="text-4xl">hello</h1>
+    </div>
+  );
+}
+
+export default App;
