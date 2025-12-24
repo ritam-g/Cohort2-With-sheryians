23ef8f9230d56b798a52ea1d86f0e683489389fa
ritam-g <ritammaty2006@gmail.com>
2025-12-24 23:38:22 +0530

    learing about useState

--- REACT/FromDay-70-to-all/vite-project/src/App.jsx
+++ REACT/FromDay-70-to-all/vite-project/src/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Button from './components/Button'
-import ConditionalRandaring from './components/ConditionalRandaring'
+import React,{useState} from 'react'
 
-function App(props) {
-  let user=['men','women','trance']
-  console.log(user[0]);
-  
+function App() {
+  const [number, setnumber] = useState(0);
+  function increase() {
+    console.log("user clicked on increase");
+    number<10?setnumber(number+1):number
+    
+    
+  }
+  function Decrease() {
+    console.log("user clicked on increase");
+    number>1?number==1? number: setnumber(number-1):number
+    
+  }
   return (
-    <div className='h-screen w-screen flex items-center justify-center bg-gray-600 gap-[1rem] '>
-      
-      {user[1]==='men'? <ConditionalRandaring data={user[0]}/> : <ConditionalRandaring data={user[1]}/> }
-
+    <div className='main bg-gray-700 h-screen w-screen  flex flex-col items-center justify-center gap-[1rem] text-2xl'>
+      <h1>{number}</h1>
+      <button 
+      onClick={increase}
+       className='bg-green-400 rounded-1xl p-2 rounded-2xl'>Increse Me</button>
+      <button onClick={Decrease}
+      className='bg-red-400 rounded-1xl p-2 rounded-2xl'>Decrese Me</button>
     </div>
   )
 }
