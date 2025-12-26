20839ced8a7d71232faeb70966d3889d5280a26a
ritam-g <ritammaty2006@gmail.com>
2025-12-26 23:39:00 +0530

    day-74 form handaling and two way binding

--- REACT/FromDay-70-to-all/vite-project/src/App.jsx
+++ REACT/FromDay-70-to-all/vite-project/src/App.jsx
@@ -1,26 +1,31 @@
 import React,{useState} from 'react'
 
 function App() {
-  const [number, setnumber] = useState(0);
-  function increase() {
-    console.log("user clicked on increase");
-    number<10?setnumber(number+1):number
-    
-    
-  }
-  function Decrease() {
-    console.log("user clicked on increase");
-    number>1?number==1? number: setnumber(number-1):number
-    
+  const [userInput, setuserInput] = useState('');
+  function onSubmitt(e) {
+    e.preventDefault();
+    console.log(userInput);
+  
   }
   return (
-    <div className='main bg-gray-700 h-screen w-screen  flex flex-col items-center justify-center gap-[1rem] text-2xl'>
-      <h1>{number}</h1>
-      <button 
-      onClick={increase}
-       className='bg-green-400 rounded-1xl p-2 rounded-2xl'>Increse Me</button>
-      <button onClick={Decrease}
-      className='bg-red-400 rounded-1xl p-2 rounded-2xl'>Decrese Me</button>
+    <div  className='h-screen w-screen bg-gray-700 text-white flex gap-3 items-center justify-center '>
+      <form
+       onSubmit={function (e) {
+          onSubmitt(e)
+        }
+      }
+      >
+        <input
+        onChange={function (e) {
+          setuserInput(e.target.value)
+            }
+          }
+         
+         value={userInput}
+         type="text" placeholder='enter your name'
+         />
+        <input type="submit" value="submit"  className='p-2 border-2'/>
+      </form>
     </div>
   )
 }
