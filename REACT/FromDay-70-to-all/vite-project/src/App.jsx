f6423eaa9c026135fa9cf83d886cb49b9dcffcca
ritam-g <ritammaty2006@gmail.com>
2025-12-30 21:48:09 +0530

    day 77

--- REACT/FromDay-70-to-all/vite-project/src/App.jsx
+++ REACT/FromDay-70-to-all/vite-project/src/App.jsx
@@ -1,44 +1,11 @@
-import React, { useState } from 'react';
-import Left from './components/Left';
-import Right from './components/Right';
+import React from 'react'
 
 function App() {
-  const [name, setname] = useState('');
-  const [email, setemail] = useState('');
-  
-  let oldData = JSON.parse(localStorage.getItem('all-Data'))||[]
-  const [allData, setallData] = useState(oldData);
-  function submitClick(e) {
-    e.preventDefault();
-    oldData.push({ name, email })
-    setallData(oldData)//! this async function 
-    //! now we have to store the data in localStorage
-    localStorage.setItem("all-Data", JSON.stringify(oldData)) //! we moving old dta becuse the setall data funciton
-    // ! is asynce may be its not sotre in that time beacuse its in heap memory so we have to pass old data its 
-    // !in stacke so it will give current data 
-    setname('')
-    setemail('')
-  }
-  function rmovePerson(params) {
-    //! i have to remove person
-    
-    oldData.splice(params,1)
-    setallData(oldData)
-    localStorage.setItem("all-Data", JSON.stringify(oldData))
-  }
   return (
-    <div className='main h-screen w-screen bg-gray-700 flex items-center justify-center p-8 gap-6'>
-      {/* Pass state and setters as props for Two-Way Binding */}
-      <Left
-        name={name}
-        setName={setname}
-        email={email}
-        setEmail={setemail}
-        handleFormSubmit={submitClick}
-      />
-      <Right rmovePerson={rmovePerson} data={oldData}/>
+    <div className='bg-black'>
+      <h1>hellow world</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
