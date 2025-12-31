46de0da7f890965791633a8784646f2d29d3edf3
ritam-g <ritammaty2006@gmail.com>
2025-12-31 23:26:31 +0530

    learning on day 78

--- REACT/FromDay-70-to-all/vite-project/src/App.jsx
+++ REACT/FromDay-70-to-all/vite-project/src/App.jsx
@@ -1,26 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import Cards from './components/Cards'
-import axios from 'axios'
+import { useState } from "react";
+import React, { useEffect } from 'react'
 
 function App() {
-  const [allData, setAllData] = useState([])
-
-  async function apiCall() {
-    try {
-      const res = await axios.get('https://picsum.photos/v2/list')
-      setAllData(res.data)
-    } catch (error) {
-      console.error("API Error:", error)
-    }
-  }
-
-  useEffect(() => {
-    apiCall()
-  }, [])
-
+  const [name, setname] = useState('');
+  useEffect(function () {
+    console.log("use Effect is running ");
+    
+  },[name])//! this is dependecy it alway in array and variable will be pass on this [] array blcik 
   return (
-    <div className="main bg-black h-screen w-screen overflow-scroll overflow-x-hidden">
-      <Cards data={allData} />
+    <div className='app bg-blue-700 w-full h-screen'>
+      <input type="text"
+       
+       onChange={e=>setname(e.target.value)}
+       value={name}
+        className="text-white p-2 bg-pink-400" placeholder="enter your name" />
+      
     </div>
   )
 }
