21d2c1b2ba822c20338dfa5c05e379a446ccae4a
ritam-g <ritammaty2006@gmail.com>
2026-01-25 16:19:24 +0530

    lernign about backend

--- Backend/day89/src/app.js
+++ Backend/day89/src/app.js
@@ -1,3 +1,44 @@
-const e = require("express");
+const express = require("express");
 
-const app=e()
+const app=express()
+app.use(express.json())
+
+let notes=[]
+//! user will get data  
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        notes:notes
+    })
+})
+//! user will create notes  
+app.post('/notes',(req,res)=>{
+    let userData=req.body
+    notes.push(userData)
+    res.status(201).json({
+        messgae:"you data is created"
+    })
+})
+
+//! user will make changes  
+app.patch('/notes/:index',(req,res)=>{
+    let {name}=req.body
+    let id=req.params.index
+    notes[id].name=name
+    res.status(200).json({
+        messgae:"you data is updated"+`${id}`
+    })
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    let id=req.params.index
+    delete notes[id]
+    res.status(200).json({
+        messgae:"deleted your data sucess fully"
+    })
+})
+
+
+
+
+
+module.exports=app
