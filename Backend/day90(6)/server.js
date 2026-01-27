27c0e43e0c62795029f39f041659c6317b8ffda6
ritam-g <ritammaty2006@gmail.com>
2026-01-27 22:09:54 +0530

    server conected with db

--- Backend/day90(6)/server.js
+++ Backend/day90(6)/server.js
@@ -1,6 +1,19 @@
+const { default: mongoose } = require("mongoose");
 const app = require("./src/app");
 
+function connectServer() {
+    mongoose.connect('mongodb+srv://ritammaty2006_db_user:SuEOrJ7QLxiLV6nm@cluster0.gejk9ft.mongodb.net/?appName=Cluster0/day-6')
+    .then(()=>{
+        console.log('server conected with cluster db and ther we made day-6 db');
+        
+    })
+    .catch(err=>console.log(err)
+    )
+}
+
+//! wrok is start server and connect with databse  
 app.listen(300,()=>{
     console.log('server is running ');
+    connectServer()
     
 })
