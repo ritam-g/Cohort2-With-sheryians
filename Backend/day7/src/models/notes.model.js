9a715bc278004ab5e53d46196a35f7bcbd5f3820
ritam-g <ritammaty2006@gmail.com>
2026-01-30 16:20:58 +0530

    revision backend

--- Backend/day7/src/models/notes.model.js
+++ Backend/day7/src/models/notes.model.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
-
+//! before sending any data to db we need schema  
 const noteSchema=new mongoose.Schema({
     title:String,
     content:String
 })
-
+//! for doing operation we need model besue it hasl all fucntion and   
+//! mongoose.model return aboject of metode  
 const noteModel=mongoose.model('notes',noteSchema)
+//! this are the methode  
+// noteModel.create()
+// noteModel.find()
+// noteModel.findById()
+// noteModel.updateOne()
+// noteModel.deleteOne()
 module.exports=noteModel
