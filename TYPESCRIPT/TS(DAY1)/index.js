26958dfee1146d3f911b43409ab78877b6c5b379
ritam-g <ritammaty2006@gmail.com>
2026-03-30 23:10:17 +0530

    day 1 of ts practice

---
+++ TYPESCRIPT/TS(DAY1)/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// console.log('hellow world');
+// string,number,boolean,array,object formatting
+const userName = 'ritam das';
+const age = 22;
+const isStudent = true;
+const hobbies = ['coding', 'gaming', 'cooking'];
+// object
+const student = {
+    name: 'ritam das',
+    height: 5.8,
+    isStudent: true
+};
+console.log("username:", userName);
+console.log("age:", age);
+console.log("isStudent:", isStudent);
+console.log("hobbies:", hobbies);
+console.log("student:", student);
+//
---
+++ TYPESCRIPT/TS(DAY1)/index.ts
@@ -0,0 +1,26 @@
+// console.log('hellow world');
+
+// string,number,boolean,array,object formatting
+
+const userName: string = 'ritam das';
+const age: number = 22;
+const isStudent: boolean = true
+const hobbies: string[] = ['coding', 'gaming', 'cooking']
+// object
+
+const student:{name:string,height:number,isStudent:boolean}={
+    name:'ritam das',
+    height:5.8,
+    isStudent:true
+}
+
+
+
+
+console.log("username:",userName);
+console.log("age:",age);
+console.log("isStudent:",isStudent);
+console.log("hobbies:",hobbies);
+console.log("student:",student);
+
+//
