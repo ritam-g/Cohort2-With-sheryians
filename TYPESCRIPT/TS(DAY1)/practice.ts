8758eac0d73a19ab1367aa04fc024acdcce63ca1
ritam-g <ritammaty2006@gmail.com>
2026-03-31 20:29:10 +0530

    feat: add TypeScript practice file and generated JavaScript output for addition function

---
+++ TYPESCRIPT/TS(DAY1)/practice.ts
@@ -0,0 +1,20 @@
+// // const userName:string="Alice"
+// // const age:number=30
+
+// // console.log(`the user userName is ${userName} and age is ${age}`);
+
+
+// const arr:number[]=[1,2,3,4,5];
+// arr.push(6);
+
+// console.log(arr);
+
+
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(add(10,20));
+console.log(add(90,100));
+
+;
