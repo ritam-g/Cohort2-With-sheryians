8f53444af1a933c954270bb995d669542d4dd32c
ritam-g <ritammaty2006@gmail.com>
2026-04-02 17:03:55 +0530

    feat: refactor form state management
    - Replaced individual state variables with a single formData object to consolidate form data.
    - Added a new usename state variable for the name input field.
    - Updated handleSubmit to append new form entries to the formData array.
    - Modified input onChange handlers to use block statements for improved readability.

--- EXTRASESSION(REACTJS)/day3/vite-project/src/App.jsx
+++ EXTRASESSION(REACTJS)/day3/vite-project/src/App.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [userName, setuserName] = useState("")
+  const [formData, setformData] = useState({ userName: "", email: "", age: "" })
+  const [usename, setusename] = useState("")
   const [email, setemail] = useState("")
   const [age, setage] = useState("")
   function handleSubmit(e) {
     e.preventDefault();
     // Handle form submission logic here
-    console.log(userName, email, age);
+    setformData([...formData,{userName:usename,email:email,age:age}])
+    console.log(formData);
   }
   return (
     <main className='w-full h-screen bg-gray-700 flex items-center justify-center'>
       <form onSubmit={handleSubmit}
         className='q-60 h-70 bg-gray-600 flex flex-col gap-4 p-4 rounded-lg'>
         <label htmlFor="name"></label>
-        <input onChange={(e) => setuserName(e.target.value)}
-          type="text" id='name' placeholder='Enter your name' value={userName} />
+        <input onChange={(e) => {
+          setusename(e.target.value)
+        }}
+          type="text" id='name' placeholder='Enter your name' value={usename} />
         <label htmlFor="email"></label>
-        <input onChange={(e) => setemail(e.target.value)}
-          type="email" id='email' placeholder='Enter your email' value={email} />
+        <input onChange={(e) => {
+          setemail(e.target.value)
+        }}
+          type="email" id='email' placeholder='Enter your email' value={ email} />
         <label htmlFor="age"></label>
-        <input onChange={(e) => setage(e.target.value)}
+        <input onChange={(e) => {
+          setage(e.target.value)
+        }}
           type="number" id='age' placeholder='Enter your age' value={age} />
         <button className='bg-blue-500 text-white rounded-4xl active:scale-90' type='submit' >Submit</button>
       </form>
