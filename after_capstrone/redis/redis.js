1b68c0ac5db380ac62bcc927964f2dca5ade1af5
ritam-g <ritammaty2006@gmail.com>
2026-05-23 00:18:43 +0530

    feat(redis): implement Redis caching for user data retrieval and storage

--- after_capstrone/redis/redis.js
+++ after_capstrone/redis/redis.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import Redis from 'ioredis'
-import userModel from './models/user.js'
+import userModel from './model.js'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
 dotenv.config()
 
 const redis = new Redis({
@@ -9,6 +11,14 @@ const redis = new Redis({
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD
 })
+redis.on('connect', () => {
+    console.log('Connected to Redis')
+})
+redis.on('error', (error) => {
+    console.error('Error connecting to Redis:', error)
+})
+
+
 const connectToMongo = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL)
@@ -20,31 +30,45 @@ const connectToMongo = async () => {
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
-
+/**  
+ * if user data is present in redis cache, return it from there
+ * if not present in redis cache, fetch it from MongoDB, store it in redis cache and return the response
+ */
 app.get(`/user/:id`, async (req, res) => {
     const { id } = req.params
     try {
-        const user = await userModel.findById(id)
+        let user;
+        user = await redis.get(`user:${id}`)
+        if (user) {
+            console.log('User data retrieved from Redis cache')
+            return res.status(200).json(JSON.parse(user))
+        }
+        user = await userModel.findById(id)
         if (!user) {
             return res.status(404).json({ message: 'User not found' })
         }
+        await redis.set(`user:${id}`, JSON.stringify(user), 'EX', 3600)
+
         return res.status(200).json(user)
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' })
     }
 })
-
+/**  
+ * create a new user in MongoDB and store the user data in redis cache with an expiry time of 1 hour
+ */
 app.post('/user', async (req, res) => {
     const { name, email, password } = req.body
     try {
         const newUser = await userModel.create({ name, email, password })
+        await redis.set(`user:${newUser._id}`, JSON.stringify(newUser), 'EX', 3600)
         return res.status(201).json(newUser)
     } catch (error) {
         return  res.status(500).json({ message: 'Internal server error' })
     }
 })
 
-app.listen(process.env.PORT, async () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 3000, async () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`)
     await connectToMongo()
 })
